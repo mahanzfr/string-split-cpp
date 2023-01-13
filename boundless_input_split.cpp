e7b423de49f18c2e82d82dd813f5a2b9956bf227
mahanzfr <jck.dewson@gmail.com>
2023-01-13 16:07:36 -0500

    Boundless input split

---
+++ boundless_input_split.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string *split(string str,char delim = ' '){
+    int size = 0;
+    int init_size = (int)str.size();
+
+	for(int i=0;i<init_size;i++){
+		if(str[i] == delim) size++;
+	}
+	size++;
+
+    string *fin = new string[size+1];
+	int index = 1;
+    string tmp = "";
+	for(int i=0;i<init_size;i++){
+		if(str[i] != delim) tmp += str[i];
+		else{
+			fin[index] = tmp;
+			index++;
+			tmp = "";
+		}
+	}
+	fin[index] = tmp;
+    fin[0] = to_string(index+1);
+    tmp = "";
+    return fin;
+}
+
+int main(){
+    string input;
+    while(1){
+        cout << "input > ";
+		getline(cin, input);
+        if(input == "") continue; // ignore empty input
+		string *alt = split(input,' '); // the first element is the size of the array
+        int arr_size = stoi(alt[0]);
+        for(int i=1;i<arr_size;i++) cout << i << ": " << alt[i] << endl;
+    }
+}
